32a7d6b47d3561d8ba97de47d9eb415fb4431d18
umair <umairapi1995@gmail.com>
2022-01-28 18:05:24 +0500

    authentication urls

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,33 @@
+from re import template
+from unicodedata import name
 from django.urls import path
-
-from .views import home, register_user
+from django.contrib.auth import views as auth_views
+from .views import home, register_user, userhome
 
 urlpatterns = [
     path('', home, name='home'),
 
     path('register/', register_user, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
+    path('userhome/', userhome, name='userhome'),
+    path('logout', auth_views.LogoutView.as_view(template_name='account/logout.html'), name='logout'),
+
+    path('password-reset/' ,
+        auth_views.PasswordResetView.as_view(template_name='account/password_reset_email.html'),
+        name='password_reset'),
+    
+    path('password-reset-done/', 
+        auth_views.PasswordResetDoneView.as_view(template_name='account/password_reset_done.html'),
+        name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(template_name='account/password_reset_confirm.html'),
+        name='password_reset_confirm'),
+
+    path('password-reset-complete',
+        auth_views.PasswordResetCompleteView.as_view(template_name='account/password_reset_complete.html'),
+        name='password_reset_complete')
+
+
+
 
 ]
