cc02113fb0cc57980cfd6ca44e694138a6b2256b
umair <umairapi1995@gmail.com>
2022-01-28 13:49:06 +0500

    Created UserRegistreaionFrom

---
+++ account/forms.py
@@ -0,0 +1,9 @@
+from operator import imod
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+
+class UserRegisterationForm(UserCreationForm):
+    class Meta:
+        fields = ['username', 'email']
+        model = get_user_model()
+
