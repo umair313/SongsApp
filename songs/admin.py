d1b160be9dd4f8f9b2e01bd852375090c181c3b1
umair <umairapi1995@gmail.com>
2022-01-31 13:55:14 +0500

    register models

--- songs/admin.py
+++ songs/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Artist, Track, Genre, Playlist, Album, PlaylistTrack
 # Register your models here.
+
+admin.site.register(Artist)
+admin.site.register(Genre)
+admin.site.register(Playlist)
+admin.site.register(Album)
+admin.site.register(PlaylistTrack)
+
+@admin.register(Track)
+class TrackAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
