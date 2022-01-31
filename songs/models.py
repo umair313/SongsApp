ee3f48e1df8a4398b69077f224db4e8550c267ad
umair <umairapi1995@gmail.com>
2022-01-31 12:04:34 +0500

    set max-lengh

--- songs/models.py
+++ songs/models.py
@@ -1,7 +1,10 @@
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
 
 class Artist(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=20)
+
+
 
