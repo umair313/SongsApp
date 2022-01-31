ba97da62b7c272cde783f151ef0e935d38048f39
umair <umairapi1995@gmail.com>
2022-01-31 12:05:07 +0500

    created album model

--- songs/models.py
+++ songs/models.py
@@ -6,5 +6,6 @@ from django.db import models
 class Artist(models.Model):
     name = models.CharField(max_length=20)
 
-
-
+class Album(models.Model):
+    name = models.CharField(max_length=100)
+    album_artist = models.ForeignKey(Artist, related_name='artist')
