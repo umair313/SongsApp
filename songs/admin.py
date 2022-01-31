9641d208276e59eb150a3c6f42072512770369fe
umair <umairapi1995@gmail.com>
2022-01-31 17:40:53 +0500

    updated admin view of models

--- songs/admin.py
+++ songs/admin.py
@@ -1,15 +1,25 @@
+from csv import list_dialects
 from django.contrib import admin
 from .models import (Artist, Track, Genre,
                     Playlist, Album, PlaylistTrack)
 
 # Register your models here.
 
-admin.site.register(Artist)
-admin.site.register(Genre)
-admin.site.register(Playlist)
-admin.site.register(Album)
 admin.site.register(PlaylistTrack)
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('name', 'artist', 'created')
+
+@admin.register(Playlist)
+class PlaylistAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'created')
+
+@admin.register(Artist, Genre)
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ('name','created')
 
 @admin.register(Track)
 class TrackAdmin(admin.ModelAdmin):
+    list_display = ('title', 'artist','album','genre', 'created')
     prepopulated_fields = {'slug':('title',)}
