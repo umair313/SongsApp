cf59623cd4b3acc9bec0b868821628b05d0df255
umair <umairapi1995@gmail.com>
2022-01-31 12:38:56 +0500

    created Track model

--- songs/models.py
+++ songs/models.py
@@ -1,4 +1,4 @@
-from unicodedata import name
+from distutils.command.upload import upload
 from django.db import models
 from django.contrib.auth import get_user_model
 
@@ -17,3 +17,18 @@ class Genre(models.Model):
 class Playlist(models.Model):
     name = models.CharField(max_length=20)
     user = models.ForeignKey(get_user_model(), related_name='user')
+
+class Track(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    thumbnails = models.ImageField(upload_to='thumbnails')
+    artist = models.ForeignKey(Artist, related_name='artist')
+    album = models.ForeignKey(Album, related_name='album')
+    genre = models.ForeignKey(Genre, related_name='genre')
+
+    class Meta:
+        ordering = ['-created']
+    
+    def __str__(self):
+        return self.title
