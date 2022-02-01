78007d9a558b1612456b354026359f76d94219d5
umair <umairapi1995@gmail.com>
2022-02-01 14:23:50 +0500

    spaces

--- songs/models.py
+++ songs/models.py
@@ -4,9 +4,10 @@ from django.contrib.auth import get_user_model
 
 # Create your models here.
 
+
 class Artist(TimeStampedModel, models.Model):
     name = models.CharField(max_length=50)
-    
+
     def __str__(self):
         return self.name
 
