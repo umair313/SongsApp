ec33987ec4dc2c50f02c87580354c5e352ac2faa
umair <umairapi1995@gmail.com>
2022-02-02 18:31:35 +0500

    remove comments

--- account/models.py
+++ account/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
+
 class User(AbstractUser):
-    # custom user model to add extra fields
     following = models.ManyToManyField('songs.Artist')
 
     def __str__(self):
