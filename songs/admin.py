4b2cb49bb7e13c83621dbabb47f2b2428f0fedd3
umair <umairapi1995@gmail.com>
2022-02-02 18:41:11 +0500

    removed foreignkey ref from admin view

--- songs/admin.py
+++ songs/admin.py
@@ -7,19 +7,23 @@ from .models import (Artist, Track, Genre,
 
 admin.site.register(PlaylistTrack)
 
+
 @admin.register(Album)
 class AlbumAdmin(admin.ModelAdmin):
-    list_display = ('name', 'artist', 'created')
+    list_display = ('name', 'created')
+
 
 @admin.register(Playlist)
 class PlaylistAdmin(admin.ModelAdmin):
     list_display = ('name', 'owner', 'created')
 
+
 @admin.register(Artist, Genre)
 class ArtistAdmin(admin.ModelAdmin):
-    list_display = ('name','created')
+    list_display = ('name', 'created')
+
 
 @admin.register(Track)
 class TrackAdmin(admin.ModelAdmin):
-    list_display = ('title', 'artist','album','genre', 'created')
-    prepopulated_fields = {'slug':('title',)}
+    list_display = ('title', 'created')
+    prepopulated_fields = {'slug': ('title',)}
